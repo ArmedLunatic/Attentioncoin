a0040f0d5a2479595139c7f8100e12160c066d1b
ArmedLunatic <god0pplays@gmail.com>
2026-01-18 02:42:54 +0530

    fix webpack config

--- attention-coin/next.config.js
+++ attention-coin/next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   images: {
     domains: ['pbs.twimg.com', 'abs.twimg.com'],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        'pino-pretty': false,
+      }
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
